9ddf92122e3d1b26c797c55c50fe46e26850d26f
dont-wait <nguyentansang15042005@gmail.com>
2025-06-11 15:25:31 +0700

    feat: update, execute query

---
+++ src/qlnhanvien/pkg3layer/dao/NhanVienDao.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+
+import java.util.List;
+import qlnhanvien.pkg3layer.entity.NhanVien;
+
+/**
+ *
+ * @author dontwait
+ */
+public class NhanVienDao {
+    public static List<NhanVien> layDanhSachNhanVien() {
+        return null;
+    }
+}
